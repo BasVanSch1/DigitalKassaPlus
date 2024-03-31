839a5bea376863cfbc8791212dbd6143b8e053a8
BasVanSch <93584219+BasVanSch@users.noreply.github.com>
2024-03-31 16:13:18 +0200

    bug fixes

--- DigitalKassaPlus/Manager.cs
+++ DigitalKassaPlus/Manager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DigitalKassaPlus
 {
-    internal class Manager : User
+    internal class Manager : Employee
     {
         public Manager(int _id, string _name, string _phone, string _email, bool _isactive) :
             base(_id, _name, _phone, _email, _isactive)
